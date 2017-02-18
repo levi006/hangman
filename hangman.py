ee90749b8dda1f39fc7160759df7fda9df1963fe
Vi Le <le.vi006@gmail.com>
2017-02-18 15:30:44 -0800

    Added a settings function for user to pick difficultly, max and min length of word.

--- hangman.py
+++ hangman.py
@@ -1,15 +1,30 @@
-import random
-import requests
-import json 
+import random, requests
 
 url = 'http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words'
-payload = {'difficulty':1}
-response = requests.get(url, params = payload) 
-print response.text
-print type(response.text)
-print repr(response.text)
 
+
+# words = ['apple', 'berry']
+
+def set_difficulty_settings():
+  difficulty_level = raw_input('Please select the difficulty level, from 1-10, where 1 is easy and 10 is the most difficult.')
+  max_length = raw_input('Please select the max length of the generated word.')
+  min_length = raw_input('Please select the min length of the generated word.')
+
+  payload = {'difficulty': difficulty_level,
+            'max length': max_length,
+            'minimum length': min_length, 
+            }
+  return payload
+
+payload = set_difficulty_settings()
+response = requests.get(url, params = payload) 
 words = response.text.splitlines()
+
+
+
+ 
+
+
 word_index = random.randrange(0, len(words) -1)
 
 chosen_word = words[word_index]
@@ -26,13 +41,13 @@ won = False
 guess = 6
 
 while not won and guess > 0:
-  guessed_ltr = raw_input("Letter?: ")
+  guessed_ltr = raw_input("Guess a letter: ")
   guessed_ltr = guessed_ltr.strip().lower()
 
   #second check for double letters
   if guessed_ltr in incorrect_letters or \
   (guessed_ltr in correct_letters and correct_letters[guessed_ltr]['found']):
-    print "You've already guessed this letter--try a different one. But you still have %s guesses left." % guess
+    print "You've already guessed this letter--try a different one. You still have %s guesses left." % guess
     print ' '.join(answer)
 
   #if letter is correct
@@ -48,8 +63,6 @@ while not won and guess > 0:
     print ' '.join(answer)
     print "Correct! You now have %s guesses left." % guess  
 
-
-
   else:
     guesses_left = 6 - guess
     guess -= 1
@@ -57,11 +70,6 @@ while not won and guess > 0:
     incorrect_letters.add(guessed_ltr)
     mistakes = ' '.join(incorrect_letters)
     print "You have guessed these letters: %s " % mistakes  
-
-
-
-
-
 
 if won:
   print("Correct! You've won!")
