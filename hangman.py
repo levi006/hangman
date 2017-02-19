216b90c4f242fc7fabfc0aa7f78cf31ff9219d1e
Vi Le <le.vi006@gmail.com>
2017-02-18 22:55:56 -0800

    Wrote a initiate_board function to display dashes before any guesses are entered

--- hangman.py
+++ hangman.py
@@ -1,14 +1,24 @@
 import random, requests
 
 def set_difficulty_settings():
-    difficulty_level = raw_input('Please select the difficulty level, from 1-10, where 1 is easy and 10 is the most difficult.')
-    # max_length = raw_input('Please select the max length of the generated word.')
-    # min_length = raw_input('Please select the min length of the generated word.')
+    while True:    
+        difficulty_level = raw_input('Please select the difficulty level, from 1-10, where 1 is easy and 10 is the most difficult.')
+        print difficulty_level.decode('utf-8')
+        print type(difficulty_level.decode('utf-8'))
+        if not difficulty_level.decode('utf-8').isnumeric() and 1 <= int(difficulty_level) <= 10:
+            print "oh noes only numbers from 1-10"
+        else:
+            break
+            return difficulty_level
 
-    payload = {'difficulty': difficulty_level
-                        # 'maxLength': max_length,
-                        # 'minLength': min_length, 
+    max_length = raw_input('Please select the max length of the generated word.')
+    min_length = raw_input('Please select the min length of the generated word.')
+
+    payload = {'difficulty': difficulty_level,
+                        'maxLength': max_length,
+                        'minLength': min_length, 
                         }
+    print payload                   
     return payload
 
 def generate_word():
@@ -28,28 +38,34 @@ def eval_alpha(guess):
         raw_ltr = guess.strip().lower()
 
         if not raw_ltr.isalpha():
-            print "oh noes only letters"
+            print "oh noes only letters <-- (OMG MY BF WROTE THIS BC HE IS SO TWEE)"
         else:
             return raw_ltr
 
 def display_hangman():
-    incorrect_letters.add(legal_ltr)
-    mistakes = ' '.join(incorrect_letters)
+    guessed_letters.add(legal_ltr)
+    mistakes = ' '.join(guessed_letters)
     print "You have guessed these letters: %s " % mistakes
     print ' '.join(answer)  
     return
+
+def initiate_board(secret_word):
+    for i in range(len(secret_word)):
+        print "_ " * max(range(len(secret_word)))
+        return
 
 #Gameplay
 
 url = 'http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words'
 secret_word = generate_word()
+initiate_board(secret_word)
 #correct letters = { letter : {count, found status}}
+guessed_letters = set()
 correct_letters = {}
 answer = []
-incorrect_letters = set()
-won = False
 guess_count = 6
 guess = ''
+won = False
 
 for idx, ltr in enumerate(secret_word):
     correct_letters.setdefault(ltr, {'index':[], 'found': False})
@@ -68,12 +84,12 @@ while won == False and guess_count > 0:
     elif len(legal_ltr) != 1 and legal_ltr != secret_word:
         print "That wasn't the secret word. Try entering one letter at a time if you're not sure what the word is."
 
-    elif guess_count > 1 and legal_ltr in incorrect_letters or \
+    elif guess_count > 1 and legal_ltr in guessed_letters or \
     (legal_ltr in correct_letters and correct_letters[legal_ltr]['found']):
         print "You've already guessed this letter--try a different one. You still have %s guesses left." % guess_count
         display_hangman() 
 
-    elif guess_count == 1 and (legal_ltr in incorrect_letters or (legal_ltr in correct_letters and correct_letters[legal_ltr]['found'])):
+    elif guess_count == 1 and (legal_ltr in guessed_letters or (legal_ltr in correct_letters and correct_letters[legal_ltr]['found'])):
         print "You've already guessed this letter--try a different one. You still have one guess left."
         display_hangman() 
 
@@ -104,7 +120,8 @@ while won == False and guess_count > 0:
             display_hangman()
 
 if won:
-    print("You've won!")
+    print("Hooray!You've won!")
+    print "The secret word was " + secret_word + "!!!"
 
 else:
     print('The gallows for you! The answer was "%s". ') % secret_word
