2825b4c946781a5b68034e7ab48f2677e04fe05e
Vi Le <le.vi006@gmail.com>
2017-02-22 13:11:44 -0800

    Wrote set_is_evil for user to choose evil setting. Fixed word bank generation bug so it correctly filters out words with guessed letters.

--- hangman.py
+++ hangman.py
@@ -1,54 +1,64 @@
 import random, requests
 from collections import defaultdict
+from gallows import GALLOWS
 
 
-
-def play():
+def play(is_evil=True):
     """Lays out the general game sequence and resets a new game."""
+    
 
     while True:
-        words = get_word_list()
-        secret_word = random.choice(words)
-        result = play_round(words, secret_word)
+        set_is_evil()
+        level = set_difficulty_level()
+        words = get_word_list(level)
+        
+
+        if is_evil:
+            # For the evil variant, the list of words all has to
+            # be the same length.
+            word_lens = [len(w) for w in words]
+            word_len = random.randint(min(word_lens), max(word_lens))
+            words = [w for w in words if len(w) == word_len]
+
+        result = play_round(words, is_evil)
 
         if result:
+            print "Congratulations!"
 
-            again = raw_input("Play again? [y/n]")
-            if again.lower().startswith("n" or "q"):
-                print "Thanks for playing!"
-                break
         else:
-            print "One hot new challenge coming up!" 
+            print "Better luck next time!"
+ 
+        again = raw_input("Play again? [y/n]").lower()
+ 
+        if again.startswith("n") or again.startswith("q"):
+            print "Thanks for playing!"
+            return
+        
+        print "One hot new challenge coming up!" 
 
-    return
 
-def play_round(words, secret_word, is_evil=True):
+def play_round(words, is_evil):
     """Contains the game play logic and messaging as game progresses."""
+
+    secret_word = random.choice(words)
     
     guesses_remaining = 6
     guessed_letters = set()
-    initiate_board(secret_word)
-    draw_gallows(guesses_remaining)    
 
-    if is_evil:
-        word_bank = make_word_bank(words, secret_word)
-        print "WORD BANK: %s %s" % (len(word_bank), word_bank)
-        print "SECRET WORD " + secret_word
+    draw_gallows(guesses_remaining)    
+    display_hangman(secret_word, guessed_letters)
+
 
     while True:
 
         guessed_ltr = prompt_guess()
 
-        if is_evil and guessed_ltr in secret_word:
-            secret_word, word_bank = switch_secret_word(guessed_ltr, word_bank)        
-            print "Correct!"
-            print "WORD BANK: %s %s" % (len(word_bank), word_bank)
-            print "SECRET WORD AFTER EVIL HANGMAN " + secret_word
-
         if guessed_ltr == secret_word:
             print "Didn't need all the guesses, did you?"
             print "'Hooray!You've won! The secret word was '%s.'" % secret_word
             return True
+
+        # Validate input
 
         elif len(guessed_ltr) != 1:
             print "That wasn't the secret word. Try entering one letter at a time if you're not sure what the word is."
@@ -62,6 +72,15 @@ def play_round(words, secret_word, is_evil=True):
             print "You've already guessed this letter. " + msg
             continue
 
+        # if evil, reduce our word list
+
+        if is_evil:
+            words = generate_word_bank(guessed_ltr, words)
+            secret_word = random.choice(words)
+
+        # print "WORD BANK: %s %s" % (len(word_bank), word_bank)
+        print "SECRET WORD " + secret_word
+
         guessed_letters.add(guessed_ltr)
 
         if guessed_ltr in secret_word:
@@ -72,6 +91,7 @@ def play_round(words, secret_word, is_evil=True):
                 return True
 
         else:
+            # wrong guess
             guesses_remaining -= 1
 
             if guesses_remaining == 0:
@@ -83,23 +103,15 @@ def play_round(words, secret_word, is_evil=True):
                 msg = "Only one guess left! Make it count!"
             else:
                 msg = "Yikes! You now have %s guesses left." % guesses_remaining
+
             print msg
             draw_gallows(guesses_remaining)
+
         display_hangman(secret_word, guessed_letters)
-    return
 
-
-def make_word_bank(words, secret_word):
-    """Construct word bank of all words the same length as secret word.
-
-        >>> make_word_bank(["ab", "aa", "ba", "foozle"], "ha")
-        ['ab', 'aa', 'ba']
-    """
     
-    return [w for w in words if len(w) == len(secret_word)]    
-    
-def switch_secret_word(guessed_ltr, word_bank):
-    """Find new secret word from word bank of possibilities. 
+def generate_word_bank(guessed_ltr, words):
+    """Builds a new word bank based on a guessed letter each turn. 
 
     Given a word bank of possible words, find the list of words that include the guessed letter
     and which have the longest-set of matching locations of the letter. Choose one randomly
@@ -107,41 +119,53 @@ def switch_secret_word(guessed_ltr, word_bank):
 
         >>> wb = ["can", "con", "non", "coy", "alf", "aaa"]
         >>> switch_secret_word("n", wb)
-        ('coy', ['coy', 'alf', 'aaa'])
+        ['coy', 'alf', 'aaa']
     """
 
     word_families = defaultdict(list)
 
-    for word in word_bank:
+    for word in words:
 
         # find locations of guessed letter in word
         indices = [i for i, ltr in enumerate(word) if ltr == guessed_ltr]
         
-        # throw out words that include letter
-        if not indices: 
-            continue
-
         # {(0, 2): ["non"], (2,): ["can", "con"]}
         word_families[tuple(indices)].append(word)
 
     # find family with most words (eg ["can", "con"])
-    word_bank = max(word_families.values(), key=lambda fam: len(fam))
+ 
+    words = max(word_families.values(), key=lambda fam: len(fam))
+    print("word_bank:", words)
+ 
+    return words 
 
-    # word = random.choice(word_bank)
-    word = word_bank[0]
-
-    return (word, word_bank) 
-
-def get_word_list():
+def set_difficulty_level():
     """Choose difficulty level and get list of words from API for that level."""
 
     while True:    
         level = raw_input('Select the difficulty level (1=easiest to 10=hardest)')
 
         if level.isdigit() and 1 <= int(level) <= 10:
-            break
+            return int(level)
 
         print "Only integers from 1-10 please!"
+
+def set_is_evil():
+    """Choose difficulty level and get list of words from API for that level."""
+   
+    answer = raw_input('Hi! Welcome to Hangman! Would you like to play on evil mode?[y/n]')
+
+    if answer == 'y':
+        is_evil = True
+        print "Evil mode on! MUAHAHA!"
+    else: 
+        is_evil = False
+        print "Vanilla it is!"
+
+    return is_evil   
+
+def get_word_list(level):
+    """Choose difficulty level and get list of words from API for that level."""
 
     params = {"difficulty": level}
     URL = 'http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words'
@@ -170,16 +194,8 @@ def prompt_guess():
         else:
             return raw_ltr
 
-def initiate_board(secret_word):
-
-    for i in range(len(secret_word)):
-        print "_ " * max(range(len(secret_word)))
-        return
-
 def display_hangman(secret_word, guessed_letters):
     """Updates board with filled and empty dashes for the secret word and displays a running list of incorrectly guessed letters.
-
-
 
     """
 
@@ -192,88 +208,11 @@ def display_hangman(secret_word, guessed_letters):
 
     print " ".join(board)
     print "You've guessed: " + " ".join(sorted(guessed_letters))
-    return
+
 
 def draw_gallows(guesses_remaining):
+    print GALLOWS[guesses_remaining]
 
-    imgs = [
-'''
- _______
-|   |  \|
-    O   |
-   \|/  |
-    |   |
-   / \  |
-        |
-        |
-       ---''',
-
-'''
- _______
-|   |  \|
-    O   |
-   \|/  |
-    |   |
-   /    |
-        |
-        |
-       ---''',
-
-'''
- _______
-|   |  \|
-    O   |
-   \|/  |
-    |   |
-        |
-        |
-        |
-       ---''',
-'''
- _______
-|   |  \|
-    O   |
-    |   |
-    |   |
-        |
-        |
-        |
-       ---''',
-
-'''
- _______
-|   |  \|
-    O   |
-    |   |
-    |   |
-        |
-        |
-        |
-       ---''',
-
- '''
- _______
-|   |  \|
-        |
-        |
-        |
-        |
-        |
-        |
-       ---''',
-'''
- _______
-|      \|
-        |
-        |
-        |
-        |
-        |
-        |
-       ---''']
-
-    print imgs[guesses_remaining]
-    return
 
 if __name__ == "__main__":
     play()
